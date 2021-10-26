151e73e5474c3fcc49e27d4e1a7578ce813b6e66
Alistair Smith <hi@alistair.sh>
2021-10-26 21:23:09 +0100

    process.exit has type never

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,6 @@ server.listen(process.env.PORT ?? 5050, err => {
 	if (err) {
 		console.log('🛑 An Error has occurred!' + err);
 		process.exit(1);
-		return;
 	}
 
 	console.log('🚀 Server launched on port 5050!');
