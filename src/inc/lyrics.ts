b8ef3bb162430d45558630b0ec5be95904c508de
Ryan <46093784+Chezzer-ub@users.noreply.github.com>
2021-11-01 12:22:38 +1300

    encode uris unlike a past bonobo

--- src/inc/lyrics.ts
+++ src/inc/lyrics.ts
@@ -3,7 +3,8 @@ import cheerio from 'cheerio';
 class Lyrics {
     async search(track:string, artist:string) {
         return new Promise((resolve) => {
-            axios.get(`https://api.lyrics.ovh/v1/${artist}/${track}`)
+            let search:string = encodeURIComponent(`${artist}/${track}`);
+            axios.get(`https://api.lyrics.ovh/v1/${search}`)
             .then(async (ovh) => {
                 let result = ovh.data.lyrics;
                 result = result.trim();
@@ -18,12 +19,13 @@ class Lyrics {
                 resolve(result);
             })
             .catch(async (e) => {
-                let res = await axios.get(`https://search.azlyrics.com/search.php?q=${artist} ${track}`);
+                let lyricSearch:string = encodeURIComponent(`${artist} ${track}`);
+                let res = await axios.get(`https://search.azlyrics.com/search.php?q=${lyricSearch}`);
                 let search = cheerio.load(res.data);
                 let url = search("tr a").attr("href");
 
                 if (url) {
-                    let lyric = await axios.get(url)
+                    let lyric = await axios.get(encodeURI(url))
                     let $ = cheerio.load(lyric.data);
 
                     let result:any = $($(".col-xs-12.col-lg-8 div")[5]).html();
